e92fa73f8f428096c7f5c2212983e391daf6b8b7
owolabijunior12 <owolabijunior12@gmail.com>
2025-09-17 18:02:30 +0100

    Feat: add Post model with fields for title, slug, content, timestamps, author, category, and tags

--- myfirstdjangoproject/posts/models.py
+++ myfirstdjangoproject/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)    
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=True)
+    author = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    tags = models.CharField(max_length=200)    
+  
+
+    def __str__(self):
+        return self.title
