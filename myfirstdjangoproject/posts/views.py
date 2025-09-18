547fe29ec1c06d8a78ae69ddacf2c23afac42a80
owolabijunior12 <owolabijunior12@gmail.com>
2025-09-18 22:37:35 +0100

    Feat: enhance post listing and improve template structure with updated styles

--- myfirstdjangoproject/posts/views.py
+++ myfirstdjangoproject/posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'posts/post_list.html', {})
+    posts = Post.objects.all().order_by('-created_at');
+    return render(request, 'posts/post_list.html', {'posts': posts})
